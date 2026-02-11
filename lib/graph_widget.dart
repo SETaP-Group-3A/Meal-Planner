ca742ec4ee708a5b750806e9b29203813bf8c9ba
Untitled Programmer <blairlucas448@gmail.com>
2026-02-11 12:37:31 +0000

    Refactor ProgressGraphWidget constructor and enhance build method for clarity

--- lib/graph_widget.dart
+++ lib/graph_widget.dart
@@ -1,12 +1,23 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/widgets.dart';
 
 class ProgressGraphWidget extends StatelessWidget {
-
   final Map<double, double> dataPoints;
 
-  const ProgressGraphWidget({super.key, required this.dataPoints})
+  const ProgressGraphWidget({super.key, required this.dataPoints});
 
   @override
   Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        lineBarsData: [
+          LineChartBarData(
+            spots: dataPoints.keys
+                .map((double data) => FlSpot(data, dataPoints[data]!))
+                .toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
