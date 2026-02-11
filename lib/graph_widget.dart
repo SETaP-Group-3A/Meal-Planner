bc4e0e535f706d02f9f5478cd09756073e0e1a64
Untitled Programmer <blairlucas448@gmail.com>
2026-02-11 10:29:47 +0000

    Add ProgressGraphWidget class with dataPoints property

---
+++ lib/graph_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ProgressGraphWidget extends StatelessWidget {
+
+  final Map<double, double> dataPoints;
+
+  const ProgressGraphWidget({super.key, required this.dataPoints})
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
