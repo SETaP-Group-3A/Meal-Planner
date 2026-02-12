7bf3d072411c9f95464cab08d1f0d63d82cb552d
Untitled Programmer <blairlucas448@gmail.com>
2026-02-12 11:12:44 +0000

    Refactor ProgressGraphWidget to convert from Stateless to Stateful, enabling dynamic updates of goalData based on userData changes.

--- lib/graph_widget.dart
+++ lib/graph_widget.dart
@@ -2,28 +2,42 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_planner/graph_controller.dart';
 
-class ProgressGraphWidget extends StatelessWidget {
+class ProgressGraphWidget extends StatefulWidget {
+  final dynamic userData;
+  const ProgressGraphWidget({super.key, required this.userData});
+
+  @override
+  State<ProgressGraphWidget> createState() => _ProgressGraphWidgetState();
+}
+
+class _ProgressGraphWidgetState extends State<ProgressGraphWidget> {
+  late GraphController controller;
   late List<int> goalData;
 
-  late GraphController controller;
+  final List<String> xTitles = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
-  //Replace var when type is known
-  var userData;
-
-  ProgressGraphWidget({super.key, required this.userData}) {
-    controller = GraphController(userData); 
-    goalData = controller.updateGraph("calories"); 
+  @override
+  void initState() {
+    super.initState();
+    controller = GraphController(widget.userData);
+    goalData = controller.updateGraph("calories");
   }
 
-  final List<String> xTitles = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+  @override
+  void didUpdateWidget(covariant ProgressGraphWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.userData != oldWidget.userData) {
+      controller = GraphController(widget.userData);
+      setState(() {
+        goalData = controller.updateGraph("calories");
+      });
+    }
+  }
 
   List<FlSpot> formatData() {
     final temp = <FlSpot>[];
     for (int i = 0; i < goalData.length; i++) {
-      temp.add(FlSpot(
-        (i + 1).toDouble(),
-        goalData[i].toDouble(),
-      ));
+      temp.add(FlSpot((i + 1).toDouble(), goalData[i].toDouble()));
     }
     return temp;
   }
@@ -33,11 +47,10 @@ class ProgressGraphWidget extends StatelessWidget {
     return LineChart(
       LineChartData(
         lineBarsData: [
-          LineChartBarData(
-            spots: formatData(),
-            color: Colors.green,
-          ),
+          LineChartBarData(spots: formatData(), color: Colors.green),
         ],
+        minX: 1,
+        maxX: 7,
         titlesData: FlTitlesData(
           bottomTitles: AxisTitles(
             sideTitles: SideTitles(
@@ -47,15 +60,14 @@ class ProgressGraphWidget extends StatelessWidget {
               getTitlesWidget: xTitlesWidgets,
             ),
           ),
-          rightTitles: AxisTitles( sideTitles: SideTitles( showTitles: false )),
-          topTitles: AxisTitles( sideTitles: SideTitles( showTitles: false ))
-      )
-      )
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        ),
+      ),
     );
   }
 
   Widget xTitlesWidgets(double value, TitleMeta meta) {
-
-    return SideTitleWidget(meta: meta, child: Text(xTitles[value.toInt() - 1])); 
-    } 
+    return SideTitleWidget(meta: meta, child: Text(xTitles[value.toInt() - 1]));
   }
+}
