6077934e9e255ccb8b86170f7904f56ba2f4282b
Untitled Programmer <blairlucas448@gmail.com>
2026-02-12 10:59:48 +0000

    Refactor GraphController to remove ProgressGraphWidget instantiation and return goal data directly from updateGraph method

--- lib/graph_controller.dart
+++ lib/graph_controller.dart
@@ -1,32 +1,21 @@
-import 'package:meal_planner/graph_widget.dart';
-
 class GraphController {
 
-  //Replace var when type is known
-  var userData;
-
-  late ProgressGraphWidget graph;
-
   GraphController(data) {
-    userData = data;
-    graph = ProgressGraphWidget(goalData: []);
-
-    //should actually pull goal out of data
+    
+    //Replace with actual goal
     updateGraph("calories");
   }
 
-  void updateGraph(var goal) {
+  List<int> updateGraph(var goal) {
     switch (goal) {
       case "calories":
         //hardcoded data for now
-        graph = ProgressGraphWidget(goalData: [2, 5, 10, 5]);
-        break;
+        return [2, 5, 10, 5];
       case "savings":
-        graph = ProgressGraphWidget(goalData: [7, 5, 4]);
-        break;
+        return [7, 5, 4];
       //Continue for other goals
       default:
-        graph = ProgressGraphWidget(goalData: [1, 2, 3, 5, 6, 7]);
+        return [1, 2, 3, 5, 6, 7];
     }
   }
 }
