1ea449a9198f47fb45171c800feacb581e79c1a1
WoodhouseA <aaronjameswoodhouse@gmail.com>
2026-02-13 10:59:21 +0000

    Remove counter logic and related UI elements from MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,14 +81,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 20),
             ElevatedButton.icon(
               onPressed: () {
                 Navigator.pushNamed(context, '/shopping-list');
@@ -105,11 +90,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
