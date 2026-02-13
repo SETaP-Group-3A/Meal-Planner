809d93a25f4aa3780367e499aa913e6ce35760dd
WoodhouseA <aaronjameswoodhouse@gmail.com>
2026-02-13 10:32:36 +0000

    Refactor ShoppingList to be a StatefulWidget, enabling state management for shopping items

--- lib/shopping_list.dart
+++ lib/shopping_list.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 
-class ShoppingList extends StatelessWidget {
-  // TODO: Checklist for Shopping List Implementation
-  // [ ] 1. Update ShoppingList constructor to accept parameters:
-  //    - String recipeId
-  //    - String sortBy (e.g., 'cost', 'distance', 'nutritional_value')
-  //
+class ShoppingList extends StatefulWidget {
+  final String? recipeId;
+  final String? sortBy;
+
+  const ShoppingList({
+    super.key,
+    this.recipeId,
+    this.sortBy,
+  });
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  // [x] 4. Create state management for the shopping list
+  final List<String> shoppingItems = [];
+
   // [ ] 2. Implement specific ingredient fetching logic (Assumed to be implemented):
   //    - Function fetchIngredients(String recipeId) returning a list of potential ingredients.
   //
   // [ ] 3. Implement preference filtering logic:
   //    - Filter/Sort the fetched ingredients based on the 'sortBy' parameter.
   //    - Select the best version of each ingredient.
-  //
-  // [ ] 4. Create state management for the shopping list (convert to StatefulWidget):
-  //    - List<String> shoppingItems
-  //
-  // [ ] 5. Build the UI:
-  //    - Use a ListView.builder to display the 'shoppingItems'.
-  //    - Each item should have a checkmark/checkbox.
-  //
+
   // [ ] 6. Implement "Add Item" functionality:
   //    - Add a TextField or FloatingActionButton to allow users to add custom items.
   //
