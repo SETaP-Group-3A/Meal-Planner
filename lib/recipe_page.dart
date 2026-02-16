e6df6b8970ca85aec91b2f577382cafc2f4c5e11
Aryaan3123 <aryaandeeba@gmail.com>
2026-02-16 19:13:13 +0000

    Add Recipe class with properties and constructor for recipe page

--- lib/recipe_page.dart
+++ lib/recipe_page.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+// Class for the recipe page
+class Recipe {
+  final String name;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> allegens;
+  final int calories;
+  final Map<String, double> macros;
+  bool isFavourite;
+
+  Recipe({
+    required this.name,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.allegens,
+    required this.calories,
+    required this.macros,
+    this.isFavourite = false,
+  });
+}
