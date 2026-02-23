7ddf2877f00135493ebb65dc6710557b3f43b439
Oskar Wieczorek <oziw57@gmail.com>
2026-02-23 09:03:42 +0000

    fix formating

--- lib/mock_data.dart
+++ lib/mock_data.dart
@@ -6,13 +6,23 @@ final Map<String, List<Ingredient>> marketInventory = {
     Ingredient(name: 'Flour (Aldi)', cost: 0.80, distance: 3.0, calories: 360),
   ],
   'Milk': [
-    Ingredient(name: 'Milk (Waitrose)', cost: 1.80, distance: 1.0, calories: 45),
+    Ingredient(
+      name: 'Milk (Waitrose)',
+      cost: 1.80,
+      distance: 1.0,
+      calories: 45,
+    ),
   ],
   'Eggs': [
     Ingredient(name: 'Eggs (Value)', cost: 1.50, distance: 0.5, calories: 155),
   ],
   'Lettuce': [
-    Ingredient(name: 'Lettuce (Fresh)', cost: 1.00, distance: 0.2, calories: 15),
+    Ingredient(
+      name: 'Lettuce (Fresh)',
+      cost: 1.00,
+      distance: 0.2,
+      calories: 15,
+    ),
   ],
   'Tomatoes': [
     Ingredient(name: 'Tomatoes', cost: 1.50, distance: 0.2, calories: 18),
@@ -29,7 +39,11 @@ final List<Recipe> mockRecipes = [
   Recipe(
     id: 'r-1',
     name: 'Scrambled Eggs on Toast',
-    requiredIngredients: ['Eggs', 'Milk', 'Flour'], // flour used for toast example
+    requiredIngredients: [
+      'Eggs',
+      'Milk',
+      'Flour',
+    ], // flour used for toast example
   ),
   Recipe(
     id: 'r-2',
