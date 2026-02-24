f7ff308d5233af30705f20b55df9a517a12aa131
Untitled Programmer <blairlucas448@gmail.com>
2026-02-24 12:03:22 +0000

    Remove unused dark mode color handling from AppStyles class

--- lib/views/app_styles.dart
+++ lib/views/app_styles.dart
@@ -5,24 +5,10 @@ class AppStyles {
 
   static final double _baseFontSize = 16;
 
-  static final (Color, Color) _textColour = (Colors.black, Colors.white);
 
   static double get baseFontSize => _baseFontSize;
 
-  static TextStyle get normalText => TextStyle(fontSize: _baseFontSize, color: isDarkMode(_textColour));
-
-  static dynamic isDarkMode((dynamic, dynamic) options) {
-
-    //actually check preference
-    bool darkMode = false;
-
-    if (darkMode)
-    {
-      return options.$2;
-    }
-
-    return options.$1;
-  }
+  static TextStyle get normalText => TextStyle(fontSize: _baseFontSize);
 }
 
 TextStyle get normalText => AppStyles.normalText;
