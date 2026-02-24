c6e1b347de3a434a6f64760f7e3c1b62a86b4ec2
Untitled Programmer <blairlucas448@gmail.com>
2026-02-24 11:09:21 +0000

    Add Account and Accessibility settings screens

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -22,3 +22,27 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 }
+
+class AccountSettingsScreen extends StatelessWidget {
+  const AccountSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Account Settings')),
+      body: Center(child: Text('Account settings go here')),
+    );
+  }
+}
+
+class AccessibilitySettingsScreen extends StatelessWidget {
+  const AccessibilitySettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Accessibility Settings')),
+      body: Center(child: Text('Accessibility settings go here')),
+    );
+  }
+}
