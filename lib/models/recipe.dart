9525695ee471bd5c63619b9158050a6082fe641c
Aryaan3123 <aryaandeeba@gmail.com>
2026-03-16 15:10:50 +0000

    Add additional fields to Recipe model for enhanced data representation

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,13 +1,28 @@
+import 'dart:convert';
+
 class Recipe {
   final String id;
   final String name;
   final List<String> requiredIngredients;
+  final int cookingTime;
+  final List<String>? allergens;
+  final int? calories;
+  final Map<String, double>? macros;
+  final Map<String, double>? nutrients;
+  final String? instructions;
 
   Recipe({
     required this.id,
     required this.name,
     required this.requiredIngredients,
+    required this.cookingTime,
+    this.allergens,
+    this.calories,
+    this.macros,
+    this.nutrients,
+    this.instructions,
   });
+
 
   @override
   String toString() {
