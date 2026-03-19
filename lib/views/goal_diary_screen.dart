df2854ac98ba3c4cd40ad1da9802862b0cb2b3b9
badideainc <untitledprogrammer@gmail.com>
2026-03-19 11:19:14 +0000

    Refactor GoalDiaryScreen to include DayGoalWidget for displaying daily goals

--- lib/views/goal_diary_screen.dart
+++ lib/views/goal_diary_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 
 class GoalDiaryScreen extends StatelessWidget {
   const GoalDiaryScreen({super.key});
@@ -10,6 +11,24 @@ class GoalDiaryScreen extends StatelessWidget {
       ),
       body: const Center(
         child: Text('This is the Goal Diary Screen'),
+      ),
+    );
+  }
+}
+
+class DayGoalWidget extends StatelessWidget {
+  final String day;
+  final String goal;
+
+  const DayGoalWidget({super.key, required this.day, required this.goal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: ListTile(
+        title: Text(day),
+        subtitle: Text(goal),
       ),
     );
   }
