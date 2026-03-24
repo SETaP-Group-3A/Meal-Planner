90b4eb3cd7f70cddefa1054f568ce94a2e96b469
Untitled Programmer <blairlucas448@gmail.com>
2026-03-24 11:14:00 +0000

    Refactor GoalDiaryScreen to accept weeklyGoals as a parameter and update DayGoalWidget to manage its own state

--- lib/views/goal_diary_screen.dart
+++ lib/views/goal_diary_screen.dart
@@ -4,7 +4,9 @@ import 'package:meal_planner/repositories/goal_repository.dart';
 
 class GoalDiaryScreen extends StatefulWidget {
 
-  const GoalDiaryScreen({super.key});
+  final List<WeeklyGoals> weeklyGoals;
+
+  const GoalDiaryScreen({super.key, required this.weeklyGoals});
 
   @override
   State<GoalDiaryScreen> createState() => _GoalDiaryScreenState();
@@ -13,7 +15,6 @@ class GoalDiaryScreen extends StatefulWidget {
 class _GoalDiaryScreenState extends State<GoalDiaryScreen> {
 
   final GoalRepository repository = GoalRepository();
-  final List<WeeklyGoals> weeklyGoals = [];
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +27,7 @@ class _GoalDiaryScreenState extends State<GoalDiaryScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Diary', style: Theme.of(context).textTheme.headlineLarge),
+            const SizedBox(height: 16),
             const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -35,13 +37,14 @@ class _GoalDiaryScreenState extends State<GoalDiaryScreen> {
               ],
             ),
             Container(height: 16),
-            for (WeeklyGoals wg in weeklyGoals)
+            for (WeeklyGoals wg in widget.weeklyGoals)
               for (final goalsForWeek in wg.goals.values)
                 for (final goalItem in goalsForWeek)
                   DayGoalWidget(
                     day: 'Day ${goalItem.day + 1}',
                     goal: goalItem.value.toString(),
                   ),
+            Text("Beneath DayGoalWidgets")
           ],
         ),
       ),
@@ -49,36 +52,54 @@ class _GoalDiaryScreenState extends State<GoalDiaryScreen> {
   }
 }
 
-class DayGoalWidget extends StatelessWidget {
+class DayGoalWidget extends StatefulWidget {
   final String day;
   final String goal;
 
   const DayGoalWidget({super.key, required this.day, required this.goal});
 
   @override
+  State<DayGoalWidget> createState() => _DayGoalWidgetState();
+}
+
+class _DayGoalWidgetState extends State<DayGoalWidget> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.goal);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(day),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Row(
+          children: [
+            Text(widget.day),
+            const SizedBox(width: 12),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                ),
+                controller: _controller,
+                onChanged: (value) {},
               ),
-              controller: TextEditingController(text: goal),
-              onChanged: (value) => {},
             ),
-          ),
-        ],
-      )
+          ],
+        ),
+      ),
     );
   }
 }
