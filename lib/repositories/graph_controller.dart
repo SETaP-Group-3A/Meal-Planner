03483f4a657c8580eb94b1ab3ca1145089be4424
badideainc <untitledprogrammer@gmail.com>
2026-03-25 11:32:11 +0000

    Refactor GraphController to use userData for goal initialization and update updateGraph method to handle specific goal cases

--- lib/repositories/graph_controller.dart
+++ lib/repositories/graph_controller.dart
@@ -1,21 +1,23 @@
+import 'package:meal_planner/models/weekly_goals.dart';
+
 class GraphController {
 
-  GraphController(data) {
-    
-    //Replace with actual goal
-    updateGraph("calories");
+  List<Goal> userData;
+
+  GraphController(this.userData) {
+    updateGraph(userData[0].id);
   }
 
-  List<int> updateGraph(var goal) {
+  List<int> updateGraph(String goal) {
     switch (goal) {
       case "calories":
         //hardcoded data for now
         return [2, 5, 10, 5];
-      case "savings":
+      case "money":
         return [7, 5, 4];
       //Continue for other goals
       default:
-        return [1, 2, 3, 5, 6, 7];
+        return [];
     }
   }
 }
