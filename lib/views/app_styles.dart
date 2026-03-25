d2f211e15ec540c5daf48480911da23c08311087
badideainc <untitledprogrammer@gmail.com>
2026-03-25 12:38:35 +0000

    Add highlight styles for good and bad values in AppStyles

--- lib/views/app_styles.dart
+++ lib/views/app_styles.dart
@@ -10,4 +10,11 @@ class AppStyles {
   static TextStyle get normalText => TextStyle(fontSize: _baseFontSize);
   static TextStyle get titleText => TextStyle(fontSize: _baseFontSize + 16, fontWeight: FontWeight.bold);
   static TextStyle get subtitleText => TextStyle(fontSize: _baseFontSize + 8, fontWeight: FontWeight.w600);
+
+  static TextStyle hightlightSwitch(int value) {
+    return value >= 0 ? highlightGood : highlightBad;
+  }
+
+  static TextStyle get highlightGood => TextStyle(fontSize: _baseFontSize, color: Colors.green, fontWeight: FontWeight.bold);
+  static TextStyle get highlightBad => TextStyle(fontSize: _baseFontSize, color: Colors.red, fontWeight: FontWeight.bold);
 }
