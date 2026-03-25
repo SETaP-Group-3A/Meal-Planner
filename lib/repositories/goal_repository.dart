31b402e6d80b5eb03560fbbe1e655398114663bc
badideainc <untitledprogrammer@gmail.com>
2026-03-25 12:23:43 +0000

    Add calculateSavings method to compute savings based on current and last week's goals

--- lib/repositories/goal_repository.dart
+++ lib/repositories/goal_repository.dart
@@ -22,4 +22,13 @@ class GoalRepository {
     //hardcoded for now
     return goals.fold(0, (sum, goal) => sum + goal.value);
   }
+
+  double calculateSavings(List<Goal> goals, List<Goal> lastWeekGoals) {
+    //No last week
+    if (lastWeekGoals.isEmpty) {
+      return 0;
+    }
+    
+    return totalAmount(goals) - totalAmount(lastWeekGoals);
+  }
 }
