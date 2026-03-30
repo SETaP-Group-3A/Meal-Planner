49300fb4e9918f6f8ad6d868491769eaca1cda8a
badideainc <untitledprogrammer@gmail.com>
2026-03-30 13:08:40 +0100

    Refactor updateGraph method to return a fixed 7-element list for goal values by day

--- lib/repositories/graph_controller.dart
+++ lib/repositories/graph_controller.dart
@@ -9,7 +9,13 @@ class GraphController {
   }
 
   List<int> updateGraph(String goal) {  
-    //Will need to switch goals here in future
-    return userData.map((g) => g.id == goal ? g.value.toInt() : 0).toList();
+    // Produce a fixed 7-element list mapping days 0..6 to values for `goal`.
+    final result = List<int>.filled(7, 0);
+    for (final g in userData) {
+      if (g.id == goal && g.day >= 0 && g.day < 7) {
+        result[g.day] = g.value.toInt();
+      }
+    }
+    return result;
   }
 }
