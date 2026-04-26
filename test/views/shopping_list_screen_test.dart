41b07e0c71ae7eea38c22bd3cdf149b393683c86
WoodhouseA <aaronjameswoodhouse@gmail.com>
2026-04-26 17:03:43 +0100

    Add setup and teardown for shopping list screen tests

--- test/views/shopping_list_screen_test.dart
+++ test/views/shopping_list_screen_test.dart
@@ -5,3 +5,24 @@ import 'package:meal_planner/models/ingredient.dart';
 import 'package:meal_planner/models/shopping_list_item.dart';
 import 'package:meal_planner/shopping_list.dart';
 import 'package:meal_planner/views/shopping_list_screen.dart';
+
+void main() {
+	final shoppingList = ShoppingList();
+
+	Future<void> pumpScreen(WidgetTester tester) async {
+		await tester.pumpWidget(
+			const MaterialApp(
+				home: ShoppingListScreen(),
+			),
+		);
+		await tester.pumpAndSettle();
+	}
+
+	setUp(() {
+		shoppingList.shoppingItems.clear();
+	});
+
+	tearDown(() {
+		shoppingList.shoppingItems.clear();
+	});
+}
