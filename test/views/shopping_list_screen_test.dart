2e37060edd3f2ba28195c8fd20e42783c1ed29e0
WoodhouseA <aaronjameswoodhouse@gmail.com>
2026-04-26 17:05:06 +0100

    Add test for showing shopping item when list is preloaded

--- test/views/shopping_list_screen_test.dart
+++ test/views/shopping_list_screen_test.dart
@@ -40,4 +40,23 @@ void main() {
 
 		expect(find.text('Your shopping list is empty.'), findsOneWidget);
 	});
+
+	testWidgets('shows shopping item when list is preloaded', (tester) async {
+		shoppingList.shoppingItems.add(
+			ShoppingListItem(
+				ingredient: Ingredient(
+					name: 'Flour (Aldi)',
+					cost: 0.80,
+					distance: 3.0,
+					calories: 360,
+				),
+			),
+		);
+
+		await pumpScreen(tester);
+
+		expect(find.text('Flour (Aldi)'), findsOneWidget);
+		expect(find.text('1'), findsOneWidget);
+		expect(find.text('Your shopping list is empty.'), findsNothing);
+	});
 }
