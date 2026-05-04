96a036aac0211da88808f3faffd3d9ebe6bd20d7
badideainc <untitledprogrammer@gmail.com>
2026-05-04 12:28:53 +0100

    add tests for calculate_savings

--- test/repositories/goal_repository_test.dart
+++ test/repositories/goal_repository_test.dart
@@ -22,4 +22,35 @@ void main() {
       expect(total, 350.0);
     });
   });
+  group("Goal Repository - Savings Amount", () {
+    test("No stored goals", () {
+      GoalRepository goalRepository = GoalRepository();
+
+      double total = goalRepository.calculateSavings([], []);
+      expect(total, 0);
+    });
+    test("No previous week goals", () {
+      GoalRepository goalRepository = GoalRepository();
+
+      double total = goalRepository.calculateSavings([
+        Goal(id: GoalType.money, day: 0, value: 100.0),
+        Goal(id: GoalType.money, day: 1, value: 50.0),
+        Goal(id: GoalType.money, day: 2, value: 200.0),
+      ], []);
+      expect(total, 0.0);
+    });
+    test("Calculate difference in goals", () {
+      GoalRepository goalRepository = GoalRepository();
+
+      double total = goalRepository.calculateSavings([
+        Goal(id: GoalType.money, day: 0, value: 100.0)
+      ], 
+      [
+        Goal(id: GoalType.money, day: 0, value: 100.0),
+        Goal(id: GoalType.money, day: 1, value: 50.0),
+        Goal(id: GoalType.money, day: 2, value: 200.0),
+      ]);
+      expect(total, 250.0);
+    });
+  });
 }
