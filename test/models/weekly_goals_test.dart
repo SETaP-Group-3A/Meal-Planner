28325404e58f6af81a05de271ad6c4ef1bdbf304
badideainc <untitledprogrammer@gmail.com>
2026-05-07 12:11:31 +0100

    use test goaltype

--- test/models/weekly_goals_test.dart
+++ test/models/weekly_goals_test.dart
@@ -18,14 +18,14 @@ void main() {
   });
   group("WeeklyGoals - Correctly add and set new goals", () {
     test("Add and set goal", () {
-      WeeklyGoals wg = WeeklyGoals();
+      WeeklyGoals wg = WeeklyGoals(type: GoalType.money);
       wg.addGoal(Goal(id: GoalType.money, day: 0, value: 100.0), 0);
       wg.setGoalValue(0, 0, 150.0);
       expect(wg.goals[0]![0].value, 150.0);
     });
 
     test("Multiple goals", () {
-      WeeklyGoals wg = WeeklyGoals();
+      WeeklyGoals wg = WeeklyGoals(type: GoalType.money);
       wg.addGoal(Goal(id: GoalType.money, day: 0, value: 100.0), 0);
       wg.addGoal(Goal(id: GoalType.money, day: 1, value: 50.0), 0);
       wg.addGoal(Goal(id: GoalType.money, day: 2, value: 200.0), 0);
@@ -34,12 +34,12 @@ void main() {
     });
 
     test("Invalid week ID", () {
-      WeeklyGoals wg = WeeklyGoals();
+      WeeklyGoals wg = WeeklyGoals(type: GoalType.money);
       expect(() => wg.addGoal(Goal(id: GoalType.money, day: 0, value: 100.0), -1), throwsArgumentError);
     });
 
     test("Invalid day", () {
-      WeeklyGoals wg = WeeklyGoals();
+      WeeklyGoals wg = WeeklyGoals(type: GoalType.money);
       expect(() => wg.setGoalValue(0, -1, 100.0), throwsArgumentError);
       expect(() => wg.setGoalValue(0, 7, 100.0), throwsArgumentError);
     });
